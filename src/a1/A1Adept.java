fa801611c6838014e1f4121f434144dd1c330368
belizyilmaz <belizyilmaz@belizs-mbp.wireless-1x.unc.edu>
2019-08-28 18:34:06 -0400

    Edited A1 Adept. A1Novice should work.

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -5,10 +5,81 @@ import java.util.Scanner;
 public class A1Adept {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int count = scan.nextInt();
+
+		String[] item = new String[count];
+		double[] price = new double[count];
 		
+		for ( int i = 0; i < item.length; i++){
+			
+			item[i] = scan.next();
+			price[i] = scan.nextDouble();
+		
+		}
+		
+		int people = scan.nextInt();
+		double sum = 0.0;
+		double big = Integer.MIN_VALUE;
+		double small = Integer.MAX_VALUE;
+		String biggestName = "";
+		String smallestName = "";
+		
+		for ( int j = 0; j < people; j++){
+			
+			String first = new String(scan.next());
+			String last = new String(scan.next());
+			int howManyItems = scan.nextInt();
+			double total = 0.0;
+		
+			for ( int a = 0; a < howManyItems; a++){
+		
+				int quantity = scan.nextInt();
+				String itemName = scan.next();
+				int index = 0;
+		
+				for ( int b = 0; b < item.length; b++){
+				
+					if ( item[b].equals(itemName)){
+			
+						index = b;
+						}
+				}
+				
+				total += quantity * price[index];
+			}
+			
+			sum += total;
+			
+			if (total > big){
+			
+				big = total;
+				biggestName = first + " " + last;
+			}
+			
+			if (total < small){
+			
+				small = total;
+				smallestName = first + " " + last;
+			}
+			
+		
+		
+		}
+		
+		scan.close();
+		
+		double average = sum / (double)people;
+		
+		System.out.println("Biggest: " + biggestName + " " + "(" + String.format("%.2f", (double) big) + ")");
+		System.out.println("Smallest: " + smallestName + " " + "(" + String.format("%.2f", (double) small) + ")");
+		System.out.println("Average: " + String.format("%.2f", (double) average));
+
 	}
+	
 }
+
+
+
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -6,9 +6,43 @@ public class A1Novice {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		
+		String [] firstLast = new String[count];
+		
+		for (int i=0; i<firstLast.length; i++) {
+			firstLast[i] = sc.next();
+			
+			int countItems = sc.nextInt();
+			int [] howManyEachItem = new int [countItems];
+			String [] item = new String[countItems];
+			Double[] price = new Double [countItems];
+			
+			
+			for (int i1=0; i1<item.length; i1++) {
+				
+				howManyEachItem[i1] = sc.nextInt();
+				
+				item[i1] = sc.next();
+				
+				price[i1] = sc.nextDouble();
 
-		// Your code follows here.
+				}
+			
+		}
+
+		sc.close();
+		
+		for(String s:firstLast){
+
+			 System.out.println(s + ": ");
+			 //+ String.format("%.2f", ((double) sum)));
+			 
+		   }
 		
 	}
-}
+
+	}
+
