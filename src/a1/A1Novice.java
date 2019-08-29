994ec843b830e304ddf6770ea0a535365fa5dc53
belizyilmaz <belizyilmaz@belizs-mbp.wireless-1x.unc.edu>
2019-08-29 16:04:47 -0400

    Edited A1Novice.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -1,48 +1,82 @@
 package a1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class A1Novice {
-
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
 		int count = sc.nextInt();
-		
-		String [] firstLast = new String[count];
-		
-		for (int i=0; i<firstLast.length; i++) {
-			firstLast[i] = sc.next();
+		String [] first= new String[count];
+		String [] last= new String[count];
+				
+		ArrayList<Double> sum = new ArrayList<Double>(count);
+
+		//double sum = 0.0;
+
+		for (int i=0; i<count; i++) {
+			
+			first[i] = sc.next();
+			last[i] = sc.next();
 			
 			int countItems = sc.nextInt();
+
 			int [] howManyEachItem = new int [countItems];
 			String [] item = new String[countItems];
-			Double[] price = new Double [countItems];
+			double[] price = new double [countItems];
 			
-			
+			double [] total = new double[countItems];
+			//double total = 0.0;
+
 			for (int i1=0; i1<item.length; i1++) {
-				
+										
 				howManyEachItem[i1] = sc.nextInt();
 				
 				item[i1] = sc.next();
 				
 				price[i1] = sc.nextDouble();
+				
+				total[i1] += howManyEachItem[i1] * price[i1];
 
+				
+				
 				}
+			
+			sum.add(findSum(total));
+		// sum = total;
 			
 		}
 
 		sc.close();
-		
-		for(String s:firstLast){
 
-			 System.out.println(s + ": ");
-			 //+ String.format("%.2f", ((double) sum)));
-			 
-		   }
-		
+		for(int i1=0; i1<count; i1++){			
+			
+			System.out.println(first[i1].charAt(0) + ". "  + last[i1] + ": " + String.format("%.2f", ((double) sum.get(i1))));
+
+		}
 	}
 
+	/*private static String[] combineString(String[] first, String[] last) {
+		
+		int length = first.length + last.length;
+        String[] result = new String[length];
+        System.arraycopy(first, 0, result, 0, first.length);
+        System.arraycopy(last, 0, result, first.length, last.length);
+        return result;
+		
+	}*/
+	
+
+	public static double findSum(double[] array) {
+	    double total = 0.0;
+	    for (int i = 0; i < array.length; i++) {
+	        total += array[i];
+	    }
+	    return total;
 	}
+	
+}
 
